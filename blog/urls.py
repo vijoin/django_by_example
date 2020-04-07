335762576c46183fb6da4950ff2911b4aa3db982
Victor Inojosa <vijoin@gmail.com>
2020-04-07 09:02:57 -0300

    Chapter 3: End of blog application. Custom template tags and filters Sitemap Feed RSS Search engine (ToDo)

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .feeds import LatestPostsFeed
 from . import views
 
 app_name = 'blog'
@@ -11,5 +12,6 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
-    path('<int:post_id>/share/', views.post_share, name="post_share")
+    path('<int:post_id>/share/', views.post_share, name="post_share"),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
